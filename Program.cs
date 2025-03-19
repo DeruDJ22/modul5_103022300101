792bffb566fc09d6972a332e0c6cf9a0d8601211
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-19 16:27:54 +0700

    menambah class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace modul5_103022300101
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            float a = 10;
+            float b = 30;
+            float c = 22;
+
+            SimpleDataBase<float> data = new SimpleDataBase<float>();
+            data.addNewData(a);
+            data.addNewData(b);
+            data.addNewData(c);
+            data.PrintAllData();
+        }
+    }
+    public class SimpleDataBase <T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputData;
+
+        public SimpleDataBase()
+        {
+            storedData = new List<T>();
+            inputData = new List<DateTime>();
+        }
+
+        public void addNewData(T data)
+        {
+            storedData.Add(data);
+            inputData.Add(DateTime.Now);
+        }
+        public void PrintAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine($"Data {i} berisi {storedData[i]} yang disimpan pada waktu UTC: {inputData[i]}");
+            }
         }
     }
 }
